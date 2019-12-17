4815114f5a8677a3e9e12627e6f3fe8eb0f61053
Rahul Kumar <rahul@fusioncharts.com>
2019-12-17 19:52:16 +0530

    Committed examples!

---
+++ using_other_libraries/amcharts_exp.php
@@ -0,0 +1,27 @@
+<?php
+
+// D3 export
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Use the sdk
+use FusionExport\ExportManager;
+use FusionExport\ExportConfig;
+
+//echo  realpath(__DIR__ . '../../resources/template_amcharts.html') ;
+//return;
+
+// Instantiate the ExportConfig class and add the required configurations
+$exportConfig = new ExportConfig();
+$exportConfig->set('templateFilePath', realpath(__DIR__ . '../../resources/template_amcharts.html'));
+$exportConfig->set('type', 'jpg');
+$exportConfig->set('asyncCapture', true);
+
+// Instantiate the ExportManager class
+$exportManager = new ExportManager();
+// Call the export() method with the export config
+$files = $exportManager->export($exportConfig, '.', true);
+
+foreach ($files as $file) {
+    echo $file . "\n";
+}
